c56dcc07f48d0d67a26642262eb5bd7f23296882
Abtahee Ali <abtaheeali@gmail.com>
2020-07-01 14:13:53 -0400

    Hard coded data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 function App() {
-  return <div className="container"
+  return (
+    <div className="container">
+      <div class="jumbotron">
+        <h1 class="display-4">Currency Converter</h1>
+        <p class="lead">2020-07-01</p>
+      </div>
+      <div>
+        <h4>Currency (USD):</h4>
+        <ul class="list-group">
+          <li class="list-group-item">"GBP": 0.8074107143</li>
+          <li class="list-group-item">"HKD": 7.7507142857</li>
+          <li class="list-group-item">"IDR": 14265.0</li>
+          <li class="list-group-item">"ILS": 3.4557142857</li>
+          <li class="list-group-item">"DKK": 6.6542857143</li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
