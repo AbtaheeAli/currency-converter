e4c3c33f4bb6a8f9ad2649a6b82fb6776666ba5b
Abtahee Ali <abtaheeali@gmail.com>
2020-07-01 15:29:53 -0400

    inputted all currency

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,134 @@ function App() {
             <h4>GBP</h4>
             {currencyPrice.GBP}
           </li>
-          <li class="list-group-item">{currencyPrice.GBP}</li>
-          <li class="list-group-item">"IDR": 14265.0</li>
-          <li class="list-group-item">"ILS": 3.4557142857</li>
-          <li class="list-group-item">"DKK": 6.6542857143</li>
+          <li class="list-group-item">
+            <h4>HKD</h4>
+            {currencyPrice.HKD}
+          </li>
+          <li class="list-group-item">
+            <h4>IDR</h4>
+            {currencyPrice.IDR}
+          </li>
+          <li class="list-group-item">
+            <h4>ILS</h4>
+            {currencyPrice.ILS}
+          </li>
+          <li class="list-group-item">
+            <h4>DKK</h4>
+            {currencyPrice.DKK}
+          </li>
+          <li class="list-group-item">
+            <h4>INR</h4>
+            {currencyPrice.INR}
+          </li>
+          <li class="list-group-item">
+            <h4>CHF</h4>
+            {currencyPrice.CHF}
+          </li>
+          <li class="list-group-item">
+            <h4>MXN</h4>
+            {currencyPrice.MXN}
+          </li>
+          <li class="list-group-item">
+            <h4>CZK</h4>
+            {currencyPrice.CZK}
+          </li>
+          <li class="list-group-item">
+            <h4>SGD</h4>
+            {currencyPrice.SGD}
+          </li>
+          <li class="list-group-item">
+            <h4>THB</h4>
+            {currencyPrice.THB}
+          </li>
+          <li class="list-group-item">
+            <h4>HRK</h4>
+            {currencyPrice.HRK}
+          </li>
+          <li class="list-group-item">
+            <h4>EUR</h4>
+            {currencyPrice.EUR}
+          </li>
+          <li class="list-group-item">
+            <h4>MYR</h4>
+            {currencyPrice.MYR}
+          </li>
+          <li class="list-group-item">
+            <h4>NOK</h4>
+            {currencyPrice.NOK}
+          </li>
+          <li class="list-group-item">
+            <h4>CNY</h4>
+            {currencyPrice.CNY}
+          </li>
+          <li class="list-group-item">
+            <h4>BGN</h4>
+            {currencyPrice.BGN}
+          </li>
+          <li class="list-group-item">
+            <h4>PHP</h4>
+            {currencyPrice.PHP}
+          </li>
+          <li class="list-group-item">
+            <h4>PLN</h4>
+            {currencyPrice.PLN}
+          </li>
+          <li class="list-group-item">
+            <h4>ZAR</h4>
+            {currencyPrice.ZAR}
+          </li>
+          <li class="list-group-item">
+            <h4>CAD</h4>
+            {currencyPrice.CAD}
+          </li>
+          <li class="list-group-item">
+            <h4>ISK</h4>
+            {currencyPrice.ISK}
+          </li>
+          <li class="list-group-item">
+            <h4>BRL</h4>
+            {currencyPrice.BRL}
+          </li>
+          <li class="list-group-item">
+            <h4>RON</h4>
+            {currencyPrice.RON}
+          </li>
+          <li class="list-group-item">
+            <h4>NZD</h4>
+            {currencyPrice.NZD}
+          </li>
+          <li class="list-group-item">
+            <h4>TRY</h4>
+            {currencyPrice.TRY}
+          </li>
+          <li class="list-group-item">
+            <h4>JPY</h4>
+            {currencyPrice.JPY}
+          </li>
+          <li class="list-group-item">
+            <h4>RUB</h4>
+            {currencyPrice.RUB}
+          </li>
+          <li class="list-group-item">
+            <h4>KRW</h4>
+            {currencyPrice.KRW}
+          </li>
+          <li class="list-group-item">
+            <h4>USD</h4>
+            {currencyPrice.USD}
+          </li>
+          <li class="list-group-item">
+            <h4>AUD</h4>
+            {currencyPrice.AUD}
+          </li>
+          <li class="list-group-item">
+            <h4>HUF</h4>
+            {currencyPrice.HUF}
+          </li>
+          <li class="list-group-item">
+            <h4>SEK</h4>
+            {currencyPrice.SEK}
+          </li>
         </ul>
       </div>
     </div>
